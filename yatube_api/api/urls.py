74f8ddb454a2220d14d028e2c1588d2ee241855b
CapitainFan <gudokrulit@mail.ru>
2022-07-07 18:47:32 +0300

    reviw of 8 sprint

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -3,11 +3,10 @@ from rest_framework import routers
 from api.views import PostViewSet, GroupViewSet, CommentViewSet
 
 router_v1 = routers.DefaultRouter()
-router_v1.register(r'posts', PostViewSet, basename='posts')
-router_v1.register(r'groups', GroupViewSet, basename='groups')
-router_v1.register(r'posts/(?P<post_id>\d+)/comments',
-                   CommentViewSet, basename='comments'
-                   )
+router_v1.register(r'posts', PostViewSet)
+router_v1.register(r'groups', GroupViewSet)
+router_v1.register(r'posts/(?P<post_id>\d+)/comments', CommentViewSet,
+                   basename='comments')
 
 urlpatterns = [
     path('v1/', include(router_v1.urls)),
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,38 +1,43 @@
 from django.shortcuts import get_object_or_404
-from rest_framework.permissions import IsAuthenticated
+from rest_framework import permissions
 from rest_framework import viewsets
-from posts.models import Group, Post, Comment
-from .serializers import PostSerializer, GroupSerializer, CommentSerializer
+from posts.models import Group, Post
+from .serializers import CommentSerializer, GroupSerializer, PostSerializer
+
+
+class IsAuthorOrReadOnly(permissions.BasePermission):
+    message = 'Пользователь может менять только свой контент!'
+
+    def has_object_permission(self, request, view, obj):
+        return (request.method in permissions.SAFE_METHODS
+                or obj.author == request.user)
+
+
+class GroupViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
 
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
+    permission_classes = [
+        permissions.IsAuthenticated, IsAuthorOrReadOnly]
+
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
 
 
-class GroupViewSet(viewsets.ReadOnlyModelViewSet):
-    queryset = Group.objects.all()
-    serializer_class = GroupSerializer
-    permission_class = [IsAuthenticated]
-
-
 class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticated,
+                          IsAuthorOrReadOnly]
 
     def get_queryset(self):
-        post_id = self.kwargs.get('post_id')
-        queryset = Comment.objects.filter(post_id=post_id)
-        return queryset
+        post = get_object_or_404(Post, pk=self.kwargs.get('post_id'))
+        return post.comments
 
     def perform_create(self, serializer):
         post = get_object_or_404(Post, pk=self.kwargs.get('post_id'))
         serializer.save(author=self.request.user, post=post)
-
-    def perform_update(self, serializer):
-        super(CommentViewSet, self).perform_update(serializer)
-
-    def perform_destroy(self, instance):
-        instance.delete()
