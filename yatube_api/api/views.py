3acf373282f6312095068407cb9cdc63b69217a5
CapitainFan <gudokrulit@mail.ru>
2022-07-07 20:38:21 +0300

    reviw of 8 sprint

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -6,12 +6,11 @@ from .serializers import CommentSerializer, GroupSerializer, PostSerializer
 from rest_framework.permissions import IsAuthenticated
 
 
-class AreYouWorthy(permissions.BasePermission):
-    message = 'Пользователь может менять только свой контент!'
-
+class IsOwnerOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
-        return (request.method in permissions.SAFE_METHODS
-                or obj.author == request.user)
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.author == request.user
 
 
 class GroupViewSet(viewsets.ReadOnlyModelViewSet):
@@ -22,8 +21,7 @@ class GroupViewSet(viewsets.ReadOnlyModelViewSet):
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-
-    permission_classes = [IsAuthenticated, AreYouWorthy]
+    permission_classes = (IsAuthenticated, IsOwnerOrReadOnly)
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
@@ -31,7 +29,7 @@ class PostViewSet(viewsets.ModelViewSet):
 
 class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
-    permission_classes = [IsAuthenticated, AreYouWorthy]
+    permission_classes = (IsAuthenticated, IsOwnerOrReadOnly)
 
     def get_queryset(self):
         post = get_object_or_404(Post, pk=self.kwargs.get('post_id'))
